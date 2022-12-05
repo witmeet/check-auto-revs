ab3fee378c951d41ad8cd604b77dffa467f5ce42
Daniela Rus Morales <danirus@eml.cc>
2022-12-05 15:26:04 +0100

    Add the section, from scratch

--- .github/scripts/edit_pr.js
+++ .github/scripts/edit_pr.js
@@ -50,15 +50,15 @@ async function update_pr_with_reviewers(github, context, title) {
   if (auto_revs_pos > -1) {
     body = body.substring(0, auto_revs_pos);
   }
-  let new_body = body + `\n\n${title}\n\n${rev_id_str}`;
+  let new_body = body + `\n${title}\n\n${rev_id_str}`;
 
   // Clean up multiple blank lines.
-  console.log("New body:", new_body);
-  const EOL = new_body.match(/\r\n/gm) ? "\r\n" : "\n";
-  console.log("EOL:", EOL);
-  const regExp = new RegExp("(" + EOL + "){3,}", "gm");
-  new_body = new_body.replace(regExp, EOL+EOL);
-  console.log("New body after replace:", new_body);
+  // console.log("New body:", new_body);
+  // const EOL = new_body.match(/\r\n/gm) ? "\r\n" : "\n";
+  // console.log("EOL:", EOL);
+  // const regExp = new RegExp("(" + EOL + "){3,}", "gm");
+  // new_body = new_body.replace(regExp, EOL+EOL);
+  // console.log("New body after replace:", new_body);
 
   // Update the body of the PR.
   github.rest.pulls.update({
